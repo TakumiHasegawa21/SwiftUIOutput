813d25cc4fa201f9c9d185ed665afce4c61d1367
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-01-31 15:48:10 +0900

    Make Base Project

---
+++ SwiftUIOutput/SwiftUIOutput/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUIOutput
+//
+//  Created by 長谷川拓海 on 2024/01/31.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
