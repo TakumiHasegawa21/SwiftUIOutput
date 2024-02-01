b40bdc6040c34445cc0ae06a1dc845d161c4ef59
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-02-01 17:39:46 +0900

    collectionViewCellを横幅いっぱいまで広げる

--- SwiftUIOutput/SwiftUIOutput/ContentView.swift
+++ SwiftUIOutput/SwiftUIOutput/ContentView.swift
@@ -12,9 +12,11 @@ struct ContentView: View {
     let data = (1...20).map { "Item \($0)" }
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.flexible(), spacing: 16), GridItem(.flexible(), spacing: 16)], spacing: 16) {
-                ForEach(data, id: \.self) { item in
+            LazyVGrid(columns: [GridItem(.flexible())], spacing: 16) {
+                ForEach(data, id: \.self) { _ in
                     CellView()
+                        .frame(maxWidth: .infinity)
+                        .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
                 }
             }
             .padding(16)
@@ -26,7 +28,7 @@ struct CellView: View {
     var body: some View {
         VStack {
             RoundedRectangle(cornerRadius: 8)
-                .frame(width: 150, height: 150)
+                .frame(height: 150)
                 .foregroundColor(.blue)
         }
     }
