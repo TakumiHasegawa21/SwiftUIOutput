f09c5b9cdd0151157e2326879adc30c09604520c
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-08-12 23:55:12 +0900

    角丸付きのViewを作成する

--- SwiftUIOutput/SwiftUIOutput/SwiftUIOutputApp.swift
+++ SwiftUIOutput/SwiftUIOutput/SwiftUIOutputApp.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct SwiftUIOutputApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RoundedCornerExample()
         }
     }
 }
